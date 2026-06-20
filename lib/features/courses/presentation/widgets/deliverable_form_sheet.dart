48c07077d1d59e0822404a1e797ed42b347c3634
FaisalAlnasri <faisal.alnasri130@gmail.com>
2026-06-20 16:07:11 +0300

    Fix deliverable form submission to always set isClear to true

--- lib/features/courses/presentation/widgets/deliverable_form_sheet.dart
+++ lib/features/courses/presentation/widgets/deliverable_form_sheet.dart
@@ -127,7 +127,7 @@ class _DeliverableFormSheetState extends State<_DeliverableFormSheet> {
       type: _type,
       date: _date,
       weight: weight,
-      isClear: _date != null,
+      isClear: true,
     );
 
     Navigator.of(context).pop<_FormResult>(
